10744ed19d3e605c87dfdb6ca7cd96a15dcacd81
LB-Brandon <rmsdud623@gmail.com>
2024-06-03 18:35:04 +0900

    MainRepository 의 반환 타입 Flow 형으로 래핑

--- data/src/main/java/com/keunyoung/data/repository/MainRepositoryImpl.kt
+++ data/src/main/java/com/keunyoung/data/repository/MainRepositoryImpl.kt
@@ -6,18 +6,20 @@ import com.google.gson.reflect.TypeToken
 import com.keunyoung.domain.model.Product
 import com.keunyoung.domain.repository.MainRepository
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import java.io.InputStreamReader
 import javax.inject.Inject
 
 class MainRepositoryImpl @Inject constructor(
 	@ApplicationContext private val context: Context
 ) : MainRepository {
-	override fun getProductList(): List<Product> {
+	override fun getProductList(): Flow<List<Product>> = flow {
 		val inputStream = context.assets.open("product_list.json")
 		val inputStreamReader = InputStreamReader(inputStream)
 		val jsonString = inputStreamReader.readText()
 		val type = object : TypeToken<List<Product>>() {}.type
 		
-		return GsonBuilder().create().fromJson(jsonString, type)
+		emit(GsonBuilder().create().fromJson(jsonString, type))
 	}
 }
--- domain/src/main/java/com/keunyoung/domain/repository/MainRepository.kt
+++ domain/src/main/java/com/keunyoung/domain/repository/MainRepository.kt
@@ -1,7 +1,8 @@
 package com.keunyoung.domain.repository
 
 import com.keunyoung.domain.model.Product
+import kotlinx.coroutines.flow.Flow
 
 interface MainRepository {
-	fun getProductList() : List<Product>
+	fun getProductList() : Flow<List<Product>>
 }
