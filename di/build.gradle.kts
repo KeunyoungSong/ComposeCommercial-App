29ce7f7e27a4784345d80898f779922535eed51e
LB-Brandon <rmsdud623@gmail.com>
2024-06-03 14:30:00 +0900

    di 모듈 hilt 의존성 추가

--- di/build.gradle.kts
+++ di/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
 	alias(libs.plugins.androidLibrary)
 	alias(libs.plugins.jetbrainsKotlinAndroid)
+	id("kotlin-kapt")
+	id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -34,6 +36,9 @@ dependencies {
 	implementation(project(":domain"))
 	implementation(project(":presentation"))
 	implementation(project(":data"))
+	
+	implementation("com.google.dagger:hilt-android:2.49")
+	kapt("com.google.dagger:hilt-compiler:2.49")
 	
 	implementation(libs.androidx.core.ktx)
 	implementation(libs.androidx.appcompat)
