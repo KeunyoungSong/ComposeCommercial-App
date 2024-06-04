19d4a7ed69f8d013f5e3bc419cdabb5474ea5eb9
LB-Brandon <rmsdud623@gmail.com>
2024-06-04 12:47:28 +0900

    MainActivity 로드 시 column count 자동 업데이트 구현

--- presentation/src/main/java/com/keunyoung/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/keunyoung/presentation/ui/MainActivity.kt
@@ -3,15 +3,20 @@ package com.keunyoung.presentation.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.keunyoung.presentation.ui.theme.ComposeCommercialAppTheme
+import com.keunyoung.presentation.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+	
+	private val viewModel: MainViewModel by viewModels()
+	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
@@ -21,6 +26,19 @@ class MainActivity : ComponentActivity() {
 				}
 			}
 		}
+		viewModel.updateColumnCount(getColumnCount())
+	}
+	
+	private fun getColumnCount(): Int {
+		return getDisplayWithDp().toInt() / DEFAULT_COLUMN_SIZE
+	}
+	
+	private fun getDisplayWithDp(): Float {
+		return resources.displayMetrics.run { widthPixels / density }
+	}
+	
+	companion object {
+		private const val DEFAULT_COLUMN_SIZE = 160
 	}
 }
 
