13fd64832c0aaf97d126be401895f864266e7531
LB-Brandon <rmsdud623@gmail.com>
2024-06-04 12:47:08 +0900

    MainViewModel 에 화면 별 컬럼 개수 추가

--- presentation/src/main/java/com/keunyoung/presentation/viewmodel/MainViewModel.kt
+++ presentation/src/main/java/com/keunyoung/presentation/viewmodel/MainViewModel.kt
@@ -1,15 +1,30 @@
 package com.keunyoung.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.keunyoung.domain.usecase.MainUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(private val useCase: MainUseCase) : ViewModel() {
+	private val _columnCount = MutableStateFlow(DEFAULT_COLUMN_COUNT)
+	val columnCount: StateFlow<Int> = _columnCount
+	
 	val productList = useCase.getProductList()
 	
-	fun openSearchForm() {
+	fun openSearchForm() {}
 	
+	fun updateColumnCount(count: Int){
+		viewModelScope.launch {
+			_columnCount.emit(count)
+		}
+	}
+	
+	companion object {
+		private const val DEFAULT_COLUMN_COUNT = 2
 	}
 }
