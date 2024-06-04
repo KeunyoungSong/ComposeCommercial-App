2a1e31e0e98a613abf4a01abc35f660cdbbdb0e0
LB-Brandon <rmsdud623@gmail.com>
2024-06-04 14:11:09 +0900

    가로 스크롤 배너 자동 스크롤 기능

--- presentation/src/main/java/com/keunyoung/presentation/component/BannerListCard.kt
+++ presentation/src/main/java/com/keunyoung/presentation/component/BannerListCard.kt
@@ -1,12 +1,15 @@
 package com.keunyoung.presentation.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
@@ -14,15 +17,20 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
 import com.google.accompanist.pager.rememberPagerState
 import com.keunyoung.domain.model.BannerList
 import com.keunyoung.presentation.R
+import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
 fun BannerListCard(model: BannerList) {
 	val pagerState = rememberPagerState()
-	HorizontalPager(count = model.imageList.size, state = pagerState) {
+	LaunchedEffect(key1 = pagerState) {
+		autoScrollInfinity(pagerState = pagerState)
+	}
+	HorizontalPager(count = model.imageList.size, state = pagerState) { pageNumber ->
 		Card(
 			shape = RoundedCornerShape(8.dp),
 			modifier = Modifier
@@ -39,5 +47,20 @@ fun BannerListCard(model: BannerList) {
 					.aspectRatio(2f)
 			)
 		}
+		Box(
+			modifier = Modifier.fillMaxWidth(),
+		) {
+			Text("pageNumber $pageNumber")
+		}
+	}
+}
+
+@OptIn(ExperimentalPagerApi::class)
+private suspend fun autoScrollInfinity(pagerState: PagerState) {
+	while (true) {
+		delay(3000)
+		val currentPage = pagerState.currentPage
+		val nextPage = (pagerState.currentPage + 1) % pagerState.pageCount
+		pagerState.animateScrollToPage(nextPage)
 	}
 }
