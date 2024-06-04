beffe6ba25116754226d8d92edba2fc379e01a21
LB-Brandon <rmsdud623@gmail.com>
2024-06-04 16:39:01 +0900

    class 를 composable function 으로 변경

--- presentation/src/main/java/com/keunyoung/presentation/ui/main/CategoryScreen.kt
+++ presentation/src/main/java/com/keunyoung/presentation/ui/main/CategoryScreen.kt
@@ -1,3 +1,14 @@
 package com.keunyoung.presentation.ui.main
 
-class CategoryScreen {}
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import com.keunyoung.presentation.viewmodel.MainViewModel
+
+@Composable
+fun CategoryScreen(
+	viewModel: MainViewModel
+) {
+	val categoryList by viewModel.categoryList.collectAsState(initial = listOf())
+	
+}
