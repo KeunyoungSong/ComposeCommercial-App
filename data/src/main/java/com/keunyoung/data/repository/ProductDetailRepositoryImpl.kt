2ec34a3af2023b4469f2354c6e49b9db91fd4f61
LB-Brandon <rmsdud623@gmail.com>
2024-06-05 17:49:44 +0900

    상품 상세 페이지를 위한 repository 와 impl 추가

---
+++ data/src/main/java/com/keunyoung/data/repository/ProductDetailRepositoryImpl.kt
@@ -0,0 +1,18 @@
+package com.keunyoung.data.repository
+
+import com.keunyoung.data.datasource.ProductDataSource
+import com.keunyoung.domain.model.Product
+import com.keunyoung.domain.repository.ProductDetailRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class ProductDetailRepositoryImpl @Inject constructor(
+	private val dataSource: ProductDataSource
+) : ProductDetailRepository {
+	override fun getProductDetail(productId: String): Flow<Product> {
+		return dataSource.getProducts().map { products ->
+			products.filterIsInstance<Product>().first { it.productId == productId }
+		}
+	}
+}
---
+++ domain/src/main/java/com/keunyoung/domain/repository/ProductDetailRepository.kt
@@ -0,0 +1,8 @@
+package com.keunyoung.domain.repository
+
+import com.keunyoung.domain.model.Product
+import kotlinx.coroutines.flow.Flow
+
+interface ProductDetailRepository {
+	fun getProductDetail(productId: String) : Flow<Product>
+}
