c25ee25cb936facaee3889657d150b412f55d7a3
LB-Brandon <rmsdud623@gmail.com>
2024-06-05 18:55:47 +0900

    Search 기능 구현을 위한 모델과 repo interface 추가

---
+++ domain/src/main/java/com/keunyoung/domain/model/SearchKeyword.kt
@@ -0,0 +1,4 @@
+package com.keunyoung.domain.model
+
+// 키워드 외 날짜 등 확장 가능
+data class SearchKeyword(val keyword: String)
---
+++ domain/src/main/java/com/keunyoung/domain/repository/SearchRepository.kt
@@ -0,0 +1,13 @@
+package com.keunyoung.domain.repository
+
+import com.keunyoung.domain.model.Product
+import com.keunyoung.domain.model.SearchKeyword
+import kotlinx.coroutines.flow.Flow
+
+interface SearchRepository {
+	// 검색 결과 가져오기
+	suspend fun search(keyword: SearchKeyword) : Flow<List<Product>>
+	
+	// 최근 검색어 가져오기
+	fun getSearchKeywords() : Flow<List<SearchKeyword>>
+}
