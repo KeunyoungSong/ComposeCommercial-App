3fd965064dc43cc43899dcb4a58938db7be0e75d
LB-Brandon <rmsdud623@gmail.com>
2024-06-10 22:35:10 +0900

    로그인 시 프로필 사진 노출 기능 구현

--- domain/src/main/java/com/keunyoung/domain/model/AccountInfo.kt
+++ domain/src/main/java/com/keunyoung/domain/model/AccountInfo.kt
@@ -1,7 +1,7 @@
 package com.keunyoung.domain.model
 
 data class AccountInfo(
-	val tokenId: String, val name: String, val type: Type
+	val tokenId: String, val name: String, val type: Type, val profileImageUrl: String
 ) {
 	enum class Type {
 		// 다른 로그인 방식 확장 지원
--- presentation/src/main/java/com/keunyoung/presentation/ui/main/MyPageScreen.kt
+++ presentation/src/main/java/com/keunyoung/presentation/ui/main/MyPageScreen.kt
@@ -6,11 +6,16 @@ import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -18,9 +23,12 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -66,23 +74,35 @@ fun MyPageScreen(viewModel: MainViewModel, googleSignInClient: GoogleSignInClien
 			.padding(30.dp)
 	) {
 		if (accountInfo != null) {
-			Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
-				Text(
-					text = "로그인 유저: ${accountInfo?.name}",
-					textAlign = TextAlign.Start,
-					modifier = Modifier.weight(1f)
-				)
-				Button(onClick = {
-					viewModel.signOut()
-					when (accountInfo?.type) {
-						AccountInfo.Type.KAKAO -> UserApiClient.instance.logout { }
-						AccountInfo.Type.GOOGLE -> firebaseAuth.signOut()
-						else -> Unit
-					}
-				}) {
-					Text("로그아웃")
+			Image(
+				painter = rememberAsyncImagePainter(accountInfo?.profileImageUrl),
+				contentDescription = null,
+				modifier = Modifier
+					.align(Alignment.CenterHorizontally)
+					.padding(5.dp)
+					.size(100.dp)
+					.clip(CircleShape)
+				,
+				contentScale = ContentScale.Crop
+			)
+			Spacer(modifier = Modifier.height(20.dp))
+			Text(
+				text = "${accountInfo?.name}",
+				textAlign = TextAlign.Center,
+				modifier = Modifier.fillMaxWidth()
+			)
+			Spacer(modifier = Modifier.weight(1f))
+			Button(onClick = {
+				viewModel.signOut()
+				when (accountInfo?.type) {
+					AccountInfo.Type.KAKAO -> UserApiClient.instance.logout { }
+					AccountInfo.Type.GOOGLE -> firebaseAuth.signOut()
+					else -> Unit
 				}
+			}, modifier = Modifier.fillMaxWidth().padding(10.dp)) {
+				Text("로그아웃")
 			}
+			Spacer(modifier = Modifier.height(70.dp))
 		} else {
 			Button(
 				onClick = { startForResult.launch(googleSignInClient.signInIntent) },
@@ -122,9 +142,10 @@ private fun loginWithKakaoNickName(token: OAuthToken, viewModel: MainViewModel) 
 			user != null -> {
 				viewModel.signIn(
 					AccountInfo(
-						token.idToken.orEmpty(),
-						user.properties?.get("nickname").orEmpty(),
-						AccountInfo.Type.KAKAO
+						tokenId = token.idToken.orEmpty(),
+						name = user.kakaoAccount?.profile?.nickname.orEmpty(),
+						type = AccountInfo.Type.KAKAO,
+						profileImageUrl = user.kakaoAccount?.profile?.thumbnailImageUrl.orEmpty(),
 					)
 				)
 			}
@@ -145,7 +166,10 @@ private fun handleSignInResult(
 			if (task.isSuccessful) {
 				viewModel.signIn(
 					AccountInfo(
-						account.idToken.orEmpty(), account.displayName.orEmpty(), AccountInfo.Type.GOOGLE
+						tokenId = account.idToken.orEmpty(),
+						name = account.displayName.orEmpty(),
+						type = AccountInfo.Type.GOOGLE,
+						profileImageUrl = account.photoUrl.toString()
 					)
 				)
 			} else {
